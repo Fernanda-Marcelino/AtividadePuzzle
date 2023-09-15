1bb9c82793240e373edaebd59f074477d3b67c00
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-09-15 16:50:31 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-class Quadro {
-    constructor(imgL, imgA, colunas){
-        this.colunas = colunas
-        this.width = 600
-        this.height = 600
-        this.widthIP = Math.floor(imgL / this.colunas)
-        this.heightIP = Math.floor(imgA / this.colunas)
-        this.tileWidth = Math.floor(this.width/this.colunas)
-        this.tileHeight = Math.floor(this.height/this.colunas)
-    }
-}
-
-var canvas = document.getElementById ('#canvas2');
-const ctx = canvas.getContext ('2d'); 
-var Quadro
-var tileImgs = []
-var tileIndx = []
-
-
+var canvas = document.getElementById ('puzzle');
+var context = canvas.getContext('2d'); 
