3c752f79de3876164c0448d5ac416a2c2d9492c9
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-09-15 23:06:13 -0300

    primeira parte

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
 var canvas = document.getElementById ('puzzle');
 var context = canvas.getContext('2d'); 
+
+class Quadro {
+    constructor(imgW, imgH, colunas) {
+       if(Quadro._instance){
+        throw new Error("Não Pega!")
+       }
+       Quadro._instance = this
+
+        this.colunas = colunas
+        this.width = 750
+        this.height = 750
+        this.widthIP = Math.floor (imgW / this.colunas)
+        this.height = Math.floor (imgH/this.colunas)
+        this.tileWidth = Math.floor (this.width / this.colunas)
+        this.tileHeight = Math.floor (this.height / this.colunas)
+    }
+}
+var board 
+var tileImgs=[]
+var tileIds=[]
+var shuffleIds=[]
+
+let cat= new Image()
+cat.src= "img/cat.jpg"
+cat.onload=cutImageintoPieces
+cat.onload=()=>{
+    context.drawImage(cat,0,0)
+}
+function cutImageintoPieces(){
+    board=new Quadro (this.naturalWidth, this.naturalHeight, 3)
+    canvas.width = board.width
+    canvas.height = board.height
+}
